0b33fc339887820393f64c4403bf2ddf918b63f9
Prashanth G <prashanth.g@belong.co>
2020-11-19 20:26:27 +0530

    basic http server

---
+++ http-server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+	if err != nil {
+		log.Fatal("error Starting Server :", err)
+
+	}
+}
